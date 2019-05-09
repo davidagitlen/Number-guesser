f45af9b1cb0b5783dae4ba33404ae8644b63da20
Chris Lane <pankokrumbs@gmail.com>
2019-05-09 16:11:51 -0600

    End of day commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var updateButton = document.querySelector("#update-button");
+    // Min and Max parameters ---> Generate randomNum
+
+        var updateButton = document.querySelector("#update-button");
         var minInput = document.querySelector("#min-input-field");
         var minOutput = document.querySelector("#min-output");
         var maxInput = document.querySelector("#max-input-field");
@@ -8,4 +10,24 @@ var updateButton = document.querySelector("#update-button");
             minOutput.innerText = newMinOutput;
             var newMaxOutput = maxInput.value;
             maxOutput.innerText = newMaxOutput;
+
+            randomNum = Math.floor(Math.random() * (newMaxOutput - newMinOutput + 1) +1);
+
+        return randomNum;
+
         });
+
+
+        // Challenger 1 and 2 input and output
+
+
+    var challengerOneName = document.querySelector("#challenger-1-name");
+    var challengerOneGuess = document.querySelector("#challenger-1-guess");
+    var challengerTwoName = document.querySelector("#challenger-2-name");
+    var challengerTwoName = document.querySelector("#challenger-2-guess");
+    var submitButton = document.querySelector("#submit-guess-button");
+
+    submitButton.addEventListener("click", function (){
+        var 
+
+    })
